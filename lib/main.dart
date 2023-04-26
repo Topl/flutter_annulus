f2d13fb617f7544515a828786a4d1696e6b7be59
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-04-26 14:14:41 -0600

    Example routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_annulus/extensions/widget_extensions.dart';
 import 'package:flutter_annulus/presentation/header.dart';
 import 'package:flutter_annulus/presentation/layout.dart';
 import 'package:flutter_annulus/widgets/hero_metrics/main.dart';
@@ -69,7 +70,100 @@ class AnnulusRouter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return VRouter(
-      routes: [],
+      initialUrl: HomeScreen.route,
+      routes: [
+        VWidget(
+          path: HomeScreen.route,
+          widget: const HomeScreen(),
+        ),
+        VNester(
+          path: '',
+          widgetBuilder: (Widget child) => SlideLeftBuilder(child: child),
+          buildTransition: (animation, _, child) => slideLeftTransition(animation, child),
+          nestedRoutes: [
+            VWidget(
+              path: TransactionDetailsScreen.route,
+              widget: const TransactionDetailsScreen(),
+            ),
+            VWidget(
+              path: BlockDetailsScreen.route,
+              widget: const BlockDetailsScreen(),
+            ),
+            VWidget(
+              path: AddNetworkScreen.route,
+              widget: const AddNetworkScreen(),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
+
+class HomeScreen extends StatelessWidget {
+  static const route = '/';
+  const HomeScreen({
+    Key? key,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class TransactionDetailsScreen extends StatelessWidget {
+  static const route = '/transaction/:id';
+
+  const TransactionDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class BlockDetailsScreen extends StatelessWidget {
+  static const route = '/block/:id';
+
+  const BlockDetailsScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SlideLeftBuilder extends StatelessWidget {
+  final Widget child;
+  const SlideLeftBuilder({
+    required this.child,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class AddNetworkScreen extends StatelessWidget {
+  static const route = '/add_network';
+
+  const AddNetworkScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+SlideTransition slideLeftTransition(Animation<double> animation, Widget child) => SlideTransition(
+      position: Tween<Offset>(
+        begin: Offset(1, 0),
+        end: Offset.zero,
+      ).animate(animation),
+      child: child,
+    );
