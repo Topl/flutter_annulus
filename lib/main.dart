5b2dc379c160982b63b074861f84579ef2547072
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-04-27 14:06:10 -0600

    Reorg of files

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_annulus/home/screen/home_screen.dart';
 import 'package:flutter_annulus/shared/utils/transitions.dart';
+import 'package:flutter_annulus/shared/widgets/slide_left_builder.dart';
 import 'package:vrouter/vrouter.dart';
 
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -37,18 +38,5 @@ class AnnulusRouter extends StatelessWidget {
         ),
       ],
     );
-  }
-}
-
-class SlideLeftBuilder extends StatelessWidget {
-  final Widget child;
-  const SlideLeftBuilder({
-    required this.child,
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
