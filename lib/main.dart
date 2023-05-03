94a9e1d1edd015b1de974bc646cb4ceecd1c4ed0
Kelz <makamanekj@gmail.com>
2023-05-03 13:53:55 +0200

    feat(ANNULUS-97): new folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_annulus/home/screen/home_screen.dart';
 import 'package:flutter_annulus/shared/utils/transitions.dart';
 import 'package:flutter_annulus/shared/widgets/slide_left_builder.dart';
+import 'package:flutter_annulus/home/screen/transaction_table_screen.dart';
 import 'package:vrouter/vrouter.dart';
-import 'package:flutter_annulus/presentation/header.dart';
-import 'package:flutter_annulus/presentation/layout.dart';
-import 'package:flutter_annulus/presentation/transaction_table.dart';
 
-import 'package:flutter_annulus/widgets/transactions/transactions.dart';
-import 'widgets/block_slider.dart';
-
-import 'package:flutter_annulus/providers/app_theme_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
@@ -36,28 +30,13 @@ class AnnulusRouter extends StatelessWidget {
         VNester(
           path: '',
           widgetBuilder: (Widget child) => SlideLeftBuilder(child: child),
-          buildTransition: (animation, _, child) => slideLeftTransition(animation, child),
-          nestedRoutes: const [
-            '/transactions': (context) => CustomLayout(
-              header: Header(
-                logoAsset: colorTheme == ColorMode.light
-                    ? 'images/logo.svg'
-                    : 'images/logo_dark.svg',
-                onSearch: () {},
-                onDropdownChanged: (String value) {},
-              ),
-              content: Column(
-                children: [
-                  TransactionTable(),
-                ],
-              ),
-              footer: Container(
-                height: 100,
-                alignment: Alignment.center,
-                child: const Text("footer"),
-              ),
+          buildTransition: (animation, _, child) =>
+              slideLeftTransition(animation, child),
+          nestedRoutes: [
+            VWidget(
+              path: TransactionTableScreen.route, // Transaction table screen
+              widget: TransactionTableScreen(),
             ),
-            /// TODO: Add Tansaction Details Screen
             /// TODO: Add Block Details Screen
             /// TODO: Add New Chain Screen
           ],
