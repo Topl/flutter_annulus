17bbfcecde7f2e7f3c3bad3e8699dd690a34447a
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-05-04 10:58:23 -0600

    First commit of provider

---
+++ lib/transactions/providers/transactions_provider.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_annulus/transactions/models/transaction.dart';
+import 'package:flutter_annulus/transactions/models/transaction_status.dart';
+import 'package:flutter_annulus/transactions/models/transaction_type.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+/// TODO: This provider is just mock data currently
+/// It is also untested, so it may not work in practice
+final transactionsProvider = StateNotifierProvider<TransactionsNotifier, AsyncValue<List<Transaction>>>((ref) {
+  return TransactionsNotifier();
+});
+
+class TransactionsNotifier extends StateNotifier<AsyncValue<List<Transaction>>> {
+  TransactionsNotifier() : super(const AsyncLoading()) {
+    getTransactions();
+  }
+
+  /// This method is used to get the list of transactions
+  /// and update the state of the provider
+  Future<void> getTransactions() async {
+    state = const AsyncLoading();
+    final List<Transaction> transactions = [];
+    const int totalTransactions = 10;
+
+    for (int i = 0; i < totalTransactions; i++) {
+      final iString = i.toString();
+      final iDouble = i.toDouble();
+      transactions.add(
+        Transaction(
+          transactionId: iString,
+          status: TransactionStatus.confirmed,
+          block: i,
+          broadcastTimestamp: DateTime.now().millisecondsSinceEpoch,
+          confirmedTimestamp: DateTime.now().millisecondsSinceEpoch,
+          transactionType: TransactionType.transfer,
+          amount: iDouble,
+          transactionFee: iDouble,
+          senderAddress: iString,
+          receiverAddress: iString,
+          transactionSize: i,
+          proposition: iString,
+          quantity: i,
+          name: iString,
+        ),
+      );
+    }
+
+    Future.delayed(const Duration(seconds: 1), () {
+      state = AsyncData(transactions);
+    });
+  }
+
+  /// This method is used to get a single transaction
+  ///
+  /// It takes a [transactionId] as a parameter
+  /// and returns a [AsyncValue<Transaction>]
+  AsyncValue<Transaction> getSingleTransaction({
+    required String transactionId,
+  }) {
+    return state.when(
+      data: (data) => AsyncData(data.firstWhere((element) => element.transactionId == transactionId)),
+      error: (error, stakeTrace) => AsyncError(error, stakeTrace),
+      loading: () => const AsyncLoading(),
+    );
+  }
+}
