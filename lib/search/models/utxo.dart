e5199a0addcdc08161861feb3aee0c354375e228
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-05-15 16:04:00 -0600

    First Addition of Search provider

--- lib/search/models/utxo.dart
+++ lib/search/models/utxo.dart
@@ -1,4 +1,3 @@
-
 // Package imports:
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -7,9 +6,7 @@ part 'utxo.g.dart';
 
 @freezed
 class UTxO with _$UTxO {
-  const factory UTxO({
-
-  }) = _UTxO;
+  const factory UTxO() = _UTxO;
 
   factory UTxO.fromJson(Map<String, dynamic> json) => _$UTxOFromJson(json);
 }
