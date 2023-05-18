89289815f460bef8a75de953c914aab9133b45fd
Enrique Berrios <enrique.noel.berrios@gmail.com>
2023-05-18 08:22:34 -0500

    block methods

--- lib/blocks/providers/block_provider.dart
+++ lib/blocks/providers/block_provider.dart
@@ -29,21 +29,34 @@ class BlockNotifier extends StateNotifier<AsyncValue<List<Block>>> {
     getLatestBlocks(setState: true);
   }
 
-  /// TODO: Implements with dart gRPC client (target genus instance in selectedChain)
+  /// TODO: Determine how to use GRPC and GRPC Web
+  /// TODO: Determine how IDs should be represented to user
+  /// TODO: Determine what to do for epoch
+  /// TODO: Determine how size is computed
+  /// TODO: Determine how to sync more blocks
+  /// TODO: Determine what to do with withdrawal number
   /// TODO: update method so that multiple latest blocks are fetched (when this is ready)
+  /// TODO: Remove blockId or header field
   ///
   /// It takes a bool [setState]
   ///
   /// If [setState] is true, it will update the state of the provider
   /// If [setState] is false, it will not update the state of the provider
   Future<List<Block>> getLatestBlocks({bool setState = false}) async {
-    final genusClient = ref.read(genusProvider(selectedChain));
+    const tempChain = Chains.private_network;
+    final genusClient = ref.read(genusProvider(tempChain));
+
     if (setState) state = const AsyncLoading();
     final List<Block> blocks = [];
     const chainHeight = 5932;
-    const pageLimit = 10;
+    const pageLimit = 4;
+
+    // var blockRes = await genusClient.getBlockByDepth(depth: 0);
+    // print('height: ${blockRes.block.header.height}');
 
     for (int i = 0; i < pageLimit; i++) {
+      var blockRes = await genusClient.getBlockByDepth(depth: i);
+
       final iString = i.toString();
       blocks.add(
         Block(
@@ -51,10 +64,10 @@ class BlockNotifier extends StateNotifier<AsyncValue<List<Block>>> {
           header: "vytVMYVjgHDHAc7AwA2Qu7JE3gPHddaTPbFWvqb2gZu$iString",
           epoch: 243827 - i,
           size: 5432.2,
-          height: chainHeight - i,
-          slot: 10,
-          timestamp: 1683494060 + i,
-          transactionNumber: 200,
+          height: blockRes.block.header.height.toInt(),
+          slot: blockRes.block.header.slot.toInt(),
+          timestamp: blockRes.block.header.timestamp.toInt(),
+          transactionNumber: blockRes.block.fullBody.transactions.length,
           withdrawalNumber: 127,
         ),
       );
@@ -93,14 +106,24 @@ class BlockNotifier extends StateNotifier<AsyncValue<List<Block>>> {
   ///
   /// It takes a [height] as a parameter
   /// and returns a [AsyncValue<Block>]
-  AsyncValue<Block> getBlockByHeight({
+  Future<Block> getBlockByHeight({
     required int height,
-  }) {
-    return state.when(
-      data: (data) =>
-          AsyncData(data.firstWhere((element) => element.height == height)),
-      error: (error, stakeTrace) => AsyncError(error, stakeTrace),
-      loading: () => const AsyncLoading(),
+  }) async {
+    const tempChain = Chains.private_network;
+    final genusClient = ref.read(genusProvider(tempChain));
+
+    var blockRes = await genusClient.getBlockByHeight(height: height);
+
+    return Block(
+      blockId: "28EhwUBiHJ3evyGidV1WH8QMfrLF6N8UDze9Yw7jqi6w",
+      header: "vytVMYVjgHDHAc7AwA2Qu7JE3gPHddaTPbFWvqb2gZu",
+      epoch: 243827,
+      size: 5432.2,
+      height: blockRes.block.header.height.toInt(),
+      slot: blockRes.block.header.slot.toInt(),
+      timestamp: blockRes.block.header.timestamp.toInt(),
+      transactionNumber: blockRes.block.fullBody.transactions.length,
+      withdrawalNumber: 127,
     );
   }
 }
