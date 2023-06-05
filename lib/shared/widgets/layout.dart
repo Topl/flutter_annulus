926b37ae3a38ce3493f5ad841ef75958888cc23c
Kelz <makamanekj@gmail.com>
2023-06-05 15:11:15 +0200

    feat(ANNULUS-148): add new network works perfectly for mobile and tablet

--- lib/shared/widgets/layout.dart
+++ lib/shared/widgets/layout.dart
@@ -1,25 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_annulus/chain/sections/chainname_dropdown.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:responsive_framework/responsive_breakpoints.dart';
 
-class CustomLayout extends StatelessWidget {
+import '../providers/app_theme_provider.dart';
+import '../utils/theme_color.dart';
+
+class CustomLayout extends HookConsumerWidget {
   final Widget header;
   final Widget content;
   final Widget footer;
+  final Widget? mobileHeader;
 
-  const CustomLayout(
-      {super.key,
-      required this.header,
-      required this.content,
-      required this.footer});
+  const CustomLayout({
+    Key? key,
+    required this.header,
+    required this.content,
+    required this.footer,
+    this.mobileHeader,
+  }) : super(key: key);
+
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final isResponsive = ResponsiveBreakpoints.of(context).smallerThan(DESKTOP);
+    final IsTablet = ResponsiveBreakpoints.of(context).equals(TABLET);
+    final ColorMode colorTheme = ref.watch(appThemeColorProvider);
+    final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
     return Scaffold(
+      key: scaffoldKey,
+      endDrawer: Container(
+        height: 500,
+        margin: EdgeInsets.only(bottom: IsTablet ? 520 : 320),
+        color: getSelectedColor(colorTheme, 0xFFFEFEFE, 0xFF282A2C),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Drawer(
+            backgroundColor:
+                getSelectedColor(colorTheme, 0xFFFEFEFE, 0xFF282A2C),
+            width: double.infinity,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 20),
+                  child: ListTile(
+                    title: Align(
+                      alignment: Alignment.centerRight,
+                      child: IconButton(
+                        icon: const Icon(Icons.close),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Center(
+                    child: ListView(
+                      padding: const EdgeInsets.only(top: 30),
+                      children: <Widget>[
+                        ListTile(
+                          title: ChainNameDropDown(
+                            colorTheme: colorTheme,
+                            onItemSelected: () => Navigator.of(context).pop(),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+      appBar: isResponsive
+          ? AppBar(
+              iconTheme: IconThemeData(
+                color: getSelectedColor(colorTheme, 0x809E9E9E, 0xFFFEFEFE),
+              ),
+              toolbarHeight: 80,
+              backgroundColor:
+                  getSelectedColor(colorTheme, 0xFFFEFEFE, 0xFF282A2C),
+              elevation: 4,
+              leading: Padding(
+                padding: const EdgeInsets.only(left: 16.0),
+                child: SvgPicture.asset(
+                  colorTheme == ColorMode.light
+                      ? 'images/logo.svg'
+                      : 'images/logo_dark.svg',
+                ),
+              ),
+            )
+          : null,
       body: Column(
         children: [
           // Header widget
           Container(
             color: Colors.white,
-            child: header,
+            child: !isResponsive ? header : null,
           ),
           // Content widget
           Expanded(
@@ -39,41 +122,6 @@ class CustomLayout extends StatelessWidget {
             ),
           ),
         ],
-      ),
-      drawer: Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
-            ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
       ),
     );
   }
