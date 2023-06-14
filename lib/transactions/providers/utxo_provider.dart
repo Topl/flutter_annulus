b8b01785de817c2f35c7be851df3e17272abfc33
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-06-14 10:33:35 -0600

    Adding comments

--- lib/transactions/providers/utxo_provider.dart
+++ lib/transactions/providers/utxo_provider.dart
@@ -2,6 +2,24 @@ import 'package:flutter_annulus/shared/providers/mock_state_provider.dart';
 import 'package:flutter_annulus/transactions/models/utxo.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+/// The [utxoByIdProvider] retrieves the [UTxO] by its [utxoId].
+/// If [mockStateProvider] is true,
+/// the function returns a mock [UTxO] after a delay of 1 second.
+/// Otherwise, it throws an exception and still needs to be implemented
+///
+/// Example usage:
+///
+/// ```
+/// final UTxO utxo = await context.read(utxoByIdProvider('utxoId').future);
+///
+/// /// final AsyncValue<UTxO> utxoAsyncValue = useProvider(utxoByIdProvider('utxoId'));
+///
+/// return utxoAsyncValue.when(
+///   data: (utxo) => Text('UTxO: $utxo'),
+///   loading: () => CircularProgressIndicator(),
+///   error: (error, stackTrace) => Text('Error: $error'),
+/// );
+/// ```
 final utxoByIdProvider = FutureProvider.family.autoDispose<UTxO, String>((ref, utxoId) async {
   if (ref.read(mockStateProvider)) {
     return Future.delayed(
