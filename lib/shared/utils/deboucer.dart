de856c9de5747461683952af8d11128f526619b4
Kelz <makamanekj@gmail.com>
2023-06-14 17:15:36 +0200

    feat(ANNULUS-103): update search

---
+++ lib/shared/utils/deboucer.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'dart:ui';
+
+/// Debouncer class to prevent multiple searches from being performed at once.
+class Debouncer {
+  final int milliseconds;
+  VoidCallback? action;
+  Timer? _timer;
+
+  Debouncer({required this.milliseconds});
+
+  void run(VoidCallback action) {
+    if (_timer != null) {
+      _timer!.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+}
--- lib/shared/widgets/custom_search_bar.dart
+++ lib/shared/widgets/custom_search_bar.dart
@@ -1,19 +1,21 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_annulus/blocks/models/block.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_annulus/shared/providers/app_theme_provider.dart';
 import 'package:modal_side_sheet/modal_side_sheet.dart';
 import 'package:responsive_framework/responsive_framework.dart';
-
 import 'package:vrouter/vrouter.dart';
 
 import '../../blocks/sections/block_details_drawer.dart';
-import '../../search/models/search_result.dart';
 import '../../search/providers/search_provider.dart';
 import '../../transactions/sections/transaction_details_drawer.dart';
+import '../utils/deboucer.dart';
 import '../utils/theme_color.dart';
 
-class CustomSearchBar extends StatefulWidget {
+/// Custom search bar widget.
+class CustomSearchBar extends HookConsumerWidget {
   const CustomSearchBar({
     Key? key,
     required this.onSearch,
@@ -24,95 +26,82 @@ class CustomSearchBar extends StatefulWidget {
   final ColorMode colorTheme;
 
   @override
-  _SearchBarState createState() => _SearchBarState();
-}
-
-class _SearchBarState extends State<CustomSearchBar> {
-  final TextEditingController _searchController = TextEditingController();
-  bool _showSuggestions = false;
-  late final String _selectedTransactionId;
-  late final Block _selectedBlock;
-  bool isUTxO = false;
-  List<String> _filteredSuggestions = [];
-
-  void _onSearchTextChanged() {
-    final searchText = _searchController.text.trim();
-    if (searchText.isNotEmpty) {
-      final filter = _suggestions
-          .where((suggestion) =>
-              suggestion.toLowerCase().contains(searchText.toLowerCase()))
-          .toList();
-      setState(() {
-        _filteredSuggestions = filter;
-        _showSuggestions = true;
-      });
-    } else {
-      setState(() {
-        _showSuggestions = false;
-      });
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _searchController.addListener(_onSearchTextChanged);
-  }
-
-  List<String> _suggestions = [];
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final isDesktop = ResponsiveBreakpoints.of(context).equals(DESKTOP);
+    final searchController = useTextEditingController();
+    final showSuggestions = useState(false);
+    final selectedTransactionId = useState('');
+    final selectedBlock = useState<Block?>(null);
+    final isUTxO = useState(false);
+    final filteredSuggestions = useState<List<String>>([]);
+    final suggestions = useState<List<String>>([]);
+    final Debouncer searchDebouncer = Debouncer(milliseconds: 200);
 
-    final searchProvider =
-        StateNotifierProvider<SearchNotifier, AsyncValue<List<SearchResult>>>(
+    void onSearchTextChanged() {
+      final searchText = searchController.text.trim();
+      if (searchText.isNotEmpty) {
+        final filter = suggestions.value
+            .where((suggestion) =>
+                suggestion.toLowerCase().contains(searchText.toLowerCase()))
+            .toList();
+        showSuggestions.value = true;
+        filteredSuggestions.value = filter;
+      } else {
+        showSuggestions.value = false;
+      }
+    }
+
+    useEffect(() {
+      searchController.addListener(onSearchTextChanged);
+      return () {
+        searchController.removeListener(onSearchTextChanged);
+      };
+    }, []);
+
+    final searchProvider = StateNotifierProvider(
       (ref) => SearchNotifier(ref),
     );
 
-    final container = ProviderContainer();
+    final searchNotifier = ref.read(searchProvider.notifier);
 
-    final searchNotifier = container.read(searchProvider.notifier);
-
-    void searchByIdAndPrintResults(int id) async {
+// Performs a search by calling `searchNotifier.searchById` with the given ID,
+// processes the results, and updates relevant values and lists.
+    Future<void> performSearch(int id) async {
       final searchResults = await searchNotifier.searchById(id);
-      if (searchResults != null) {
-        List<String> transactions = [];
-        List<String> blocks = [];
-        List<String> utxos = [];
+      if (searchResults.isNotEmpty) {
+        final transactions = <String>[];
+        final blocks = <String>[];
+        final utxos = <String>[];
 
         for (var searchResult in searchResults) {
           searchResult.when(
             transaction: (transaction) {
               final transactionId = transaction.transactionId;
               transactions.add(transactionId);
-              setState(() {
-                _selectedTransactionId = transactionId;
-              });
+              selectedTransactionId.value = transactionId;
             },
             block: (block) {
               final blockValue = block.epoch;
               blocks.add(blockValue.toString());
-              setState(() {
-                _selectedBlock = block;
-              });
+              selectedBlock.value = block;
             },
             uTxO: (utxo) {
               // TODO: Handle UTxO result
               const utxoValue = "0x5be9d701Byd24neQfY1vXa987a";
               utxos.add(utxoValue);
-              setState(() {
-                isUTxO = true;
-              });
+              isUTxO.value = true;
             },
           );
         }
-        setState(() {
-          _suggestions = transactions + blocks + utxos;
-        });
+
+        suggestions.value = transactions + blocks + utxos;
       } else {
         print("No results found.");
       }
+    }
+
+    Future<void> searchByIdAndPrintResults(int id) async {
+      searchDebouncer.run(() => performSearch(id));
     }
 
     return SizedBox(
@@ -121,15 +110,12 @@ class _SearchBarState extends State<CustomSearchBar> {
         children: [
           TextField(
             style: TextStyle(
-              color:
-                  getSelectedColor(widget.colorTheme, 0xFF4B4B4B, 0xFF858E8E),
+              color: getSelectedColor(colorTheme, 0xFF4B4B4B, 0xFF858E8E),
             ),
-            controller: _searchController,
-            onSubmitted: (query) => widget.onSearch(),
+            controller: searchController,
+            onSubmitted: (query) => onSearch(),
             onChanged: (value) {
-              setState(() {
-                _showSuggestions = value.isNotEmpty;
-              });
+              showSuggestions.value = value.isNotEmpty;
               final valueId = int.tryParse(value);
 
               if (valueId != null) {
@@ -139,19 +125,16 @@ class _SearchBarState extends State<CustomSearchBar> {
             decoration: InputDecoration(
               hintText: 'Search by blocks, transactions, or UTxOs',
               hintStyle: TextStyle(
-                color:
-                    getSelectedColor(widget.colorTheme, 0xFF4B4B4B, 0xFF858E8E),
+                color: getSelectedColor(colorTheme, 0xFF4B4B4B, 0xFF858E8E),
                 fontFamily: 'Rational Display',
               ),
               prefixIcon: Icon(
                 Icons.search,
-                color:
-                    getSelectedColor(widget.colorTheme, 0xFFC0C4C4, 0xFF4B4B4B),
+                color: getSelectedColor(colorTheme, 0xFFC0C4C4, 0xFF4B4B4B),
               ),
               enabledBorder: OutlineInputBorder(
                 borderSide: BorderSide(
-                  color: getSelectedColor(
-                      widget.colorTheme, 0xFFC0C4C4, 0xFF4B4B4B),
+                  color: getSelectedColor(colorTheme, 0xFFC0C4C4, 0xFF4B4B4B),
                   width: 1.0,
                 ),
                 borderRadius: BorderRadius.circular(8.0),
@@ -160,14 +143,13 @@ class _SearchBarState extends State<CustomSearchBar> {
               focusColor: const Color(0xFF4B4B4B),
               focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(
-                  color: getSelectedColor(
-                      widget.colorTheme, 0xFF4B4B4B, 0xFF858E8E),
+                  color: getSelectedColor(colorTheme, 0xFF4B4B4B, 0xFF858E8E),
                   width: 1.0,
                 ),
               ),
             ),
           ),
-          if (_showSuggestions && _filteredSuggestions.isNotEmpty)
+          if (showSuggestions.value && filteredSuggestions.value.isNotEmpty)
             Stack(
               children: <Widget>[
                 Positioned(
@@ -181,23 +163,21 @@ class _SearchBarState extends State<CustomSearchBar> {
                     ),
                     child: ListView.builder(
                       shrinkWrap: true,
-                      itemCount: _filteredSuggestions.length,
+                      itemCount: filteredSuggestions.value.length,
                       itemBuilder: (context, index) {
-                        final suggestion = _filteredSuggestions[index];
+                        final suggestion = filteredSuggestions.value[index];
 
                         final displayText = suggestion.length == 6
                             ? 'Block $suggestion'
                             : suggestion;
                         return ListTile(
-                          title:
-                              Text(displayText, style: TextStyle(fontSize: 14)),
+                          title: Text(displayText,
+                              style: const TextStyle(fontSize: 14)),
                           textColor: getSelectedColor(
-                              widget.colorTheme, 0xFF4B4B4B, 0xFF858E8E),
+                              colorTheme, 0xFF4B4B4B, 0xFF858E8E),
                           onTap: () {
-                            setState(() {
-                              _searchController.text = suggestion;
-                              _showSuggestions = false;
-                            });
+                            searchController.text = suggestion;
+                            showSuggestions.value = false;
 
                             if (isDesktop && suggestion.length > 6) {
                               showModalSideSheet(
@@ -207,26 +187,27 @@ class _SearchBarState extends State<CustomSearchBar> {
                                 barrierColor: Colors.white.withOpacity(0.64),
                                 barrierDismissible: true,
                                 body: TransactionDetailsDrawer(
-                                  transactionId: _selectedTransactionId,
+                                  transactionId: selectedTransactionId.value,
                                 ),
                               );
                             } else if (!isDesktop && suggestion.length > 6) {
                               context.vRouter.to(
-                                  '/transactions_details/$_selectedTransactionId');
+                                  '/transactions_details/${selectedTransactionId.value}');
                             } else {
                               showModalSideSheet(
-                                  context: context,
-                                  ignoreAppBar: true,
-                                  width: 640,
-                                  barrierColor: getSelectedColor(
-                                          widget.colorTheme,
-                                          0xFFFEFEFE,
-                                          0xFF353739)
-                                      .withOpacity(0.64),
-                                  barrierDismissible: true,
-                                  body: BlockDetailsDrawer(
-                                    block: _selectedBlock,
-                                  ));
+                                context: context,
+                                ignoreAppBar: true,
+                                width: 640,
+                                barrierColor: getSelectedColor(
+                                  colorTheme,
+                                  0xFFFEFEFE,
+                                  0xFF353739,
+                                ).withOpacity(0.64),
+                                barrierDismissible: true,
+                                body: BlockDetailsDrawer(
+                                  block: selectedBlock.value,
+                                ),
+                              );
                             }
                           },
                         );
@@ -235,7 +216,7 @@ class _SearchBarState extends State<CustomSearchBar> {
                   ),
                 )
               ],
-            )
+            ),
         ],
       ),
     );
