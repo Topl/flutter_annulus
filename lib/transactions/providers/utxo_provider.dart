333ca2c9773dd6ecdcfc9317502a8f0451f9ac60
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-06-14 11:00:53 -0600

    Seperation into utils

--- lib/transactions/providers/utxo_provider.dart
+++ lib/transactions/providers/utxo_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_annulus/shared/providers/mock_state_provider.dart';
 import 'package:flutter_annulus/transactions/models/utxo.dart';
+import 'package:flutter_annulus/transactions/utils/utxo_utils.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 /// The [utxoByIdProvider] retrieves the [UTxO] by its [utxoId].
@@ -23,22 +24,9 @@ final utxoByIdProvider = FutureProvider.family.autoDispose<UTxO, String>((ref, u
   if (ref.read(mockStateProvider)) {
     return Future.delayed(
       const Duration(seconds: 1),
-      () => _getMockUTxO(),
+      () => getMockUTxO(),
     );
   } else {
     throw Exception('UTxO RPC calls not yet implemented');
   }
 });
-
-UTxO _getMockUTxO() {
-  return const UTxO(
-    utxoId: '0x5be9d701Byd24neQfY1vXa987a',
-    blockId: '242218',
-    changeBackAddress: '3m21ucZ0pFyvxa1by9dnE2q87e3P6ic',
-    fee: 0.1,
-    inputAmount: [5.5, 123],
-    outputAmount: [6, 0.63],
-    receiverAddress: '7bY6Dne54qMU12cz3oPF4yVx5aG6a',
-    senderAddress: '3m21ucZ0pFyvxa1by9dnE2q87e3P6ic',
-  );
-}
