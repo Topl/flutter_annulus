331dd668baf36032070ea429aeb8c10a3e1d09d7
Kelz <makamanekj@gmail.com>
2023-06-27 17:10:34 +0200

    feat(ANNULUS-160): add drawer details not showing and refactor

--- lib/shared/widgets/custom_search_bar.dart
+++ lib/shared/widgets/custom_search_bar.dart
@@ -177,7 +177,7 @@ class CustomSearchBar extends HookConsumerWidget {
                           suggestion.length > 6 &&
                           suggestion != "10x5be9d701Byd24neQfY1vXa987a") {
                         context.vRouter.to(
-                          '/transactions_details/${selectedTransactionId.value}',
+                          '/transactions_details/',
                         );
                       } else if (suggestion ==
                           "10x5be9d701Byd24neQfY1vXa987a") {
