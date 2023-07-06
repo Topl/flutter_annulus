ef41cd1a46abd6e094256bbf316ac51471650b22
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-07-06 09:46:26 -0600

    Naming update

--- lib/shared/providers/node_provider.dart
+++ lib/shared/providers/node_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter_annulus/chain/models/chains.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:topl_common/genus/services/node_grpc.dart';
 
-final genusProvider = Provider.family<NodeGRPCService, Chains>((ref, chain) {
+final nodeProvider = Provider.family<NodeGRPCService, Chains>((ref, chain) {
   final NodeGRPCService service = NodeGRPCService(host: chain.host, port: chain.port);
 
   return service;
