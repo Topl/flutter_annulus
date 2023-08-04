b0d24f6d1ca18c218d3a5be1a9889a89b1e82194
Enrique Berrios <enrique.noel.berrios@gmail.com>
2023-08-03 22:36:30 -0500

    fix selected chain

--- lib/chain/providers/selected_chain_provider.dart
+++ lib/chain/providers/selected_chain_provider.dart
@@ -6,7 +6,7 @@ import 'package:flutter_annulus/shared/services/hive/hives.dart';
 import 'package:flutter_annulus/shared/utils/hive_utils.dart';
 
 final selectedChainProvider = StateProvider<Chains>((ref) {
-  return kDebugMode ? Chains.mock : Chains.topl_mainnet;
+  return kDebugMode ? const Chains.mock() : const Chains.topl_mainnet();
 });
 
 final chainsProvider = StateNotifierProvider<ChainsNotifier, AsyncValue<List<Chains>>>((ref) {
