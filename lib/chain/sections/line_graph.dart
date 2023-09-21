4568bc70ddaa283c33d516c50c33931127597efe
Kelz <makamanekj@gmail.com>
2023-09-21 15:18:49 +0200

    feat(ANNULUS-302): adds 100 data points  time frame to

--- lib/chain/sections/line_graph.dart
+++ lib/chain/sections/line_graph.dart
@@ -1,5 +1,6 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_annulus/blocks/models/block.dart';
 import 'package:flutter_annulus/blocks/providers/block_provider.dart';
 import 'package:flutter_annulus/chain/models/chart_result.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -16,7 +17,7 @@ class LineGraph extends HookConsumerWidget {
       data: (data) {
         return ref.watch(chartProvider).when(
               data: (chart) {
-                return LineGraphContainer(chartData: chart);
+                return LineGraphContainer(chartData: chart, blockData: data);
               },
               error: (error, stack) => const Text('Oops, something unexpected happened'),
               loading: () => const Center(
@@ -34,88 +35,123 @@ class LineGraph extends HookConsumerWidget {
   }
 }
 
-class LineGraphContainer extends StatelessWidget {
-  const LineGraphContainer({super.key, required this.chartData});
+class LineGraphContainer extends HookConsumerWidget {
+  const LineGraphContainer({
+    Key? key,
+    required this.chartData,
+    required this.blockData,
+  }) : super(key: key);
 
   final ChartResult chartData;
+  final blockData;
 
   @override
-  Widget build(BuildContext context) {
-    final pointList = chartData.results.entries
-        .map((e) => FlSpot(e.key.millisecondsSinceEpoch.toDouble(), e.value.toDouble()))
-        .toList();
-
-    var maxYValue = chartData.results.entries.map((e) => e.value.toDouble()).toList().reduce(max).ceilToDouble();
-    if (maxYValue == 0) {
-      maxYValue = 1;
+  Widget build(BuildContext context, ref) {
+    final selectedTimeFrame = ref.watch(timeFrameProvider);
+    double calculateSpacing(int desiredDataPoints, double averageBlockTime) {
+      final timeFrameInSeconds = timeFrameToDuration(selectedTimeFrame).inSeconds;
+      return timeFrameInSeconds / desiredDataPoints / averageBlockTime;
     }
 
-    final maxXValue =
-        chartData.results.entries.map((e) => e.key.millisecondsSinceEpoch.toDouble()).toList().reduce(max).toDouble();
+    return Consumer(
+      builder: (context, watch, child) {
+        final blockData = ref.watch(blockProvider);
 
-    final minXValue =
-        chartData.results.entries.map((e) => e.key.millisecondsSinceEpoch.toDouble()).toList().reduce(min).toDouble();
+        return blockData.when(
+          data: (blocks) {
+            final pointList = chartData.results.entries
+                .map((e) => FlSpot(e.key.millisecondsSinceEpoch.toDouble(), e.value.toDouble()))
+                .toList();
 
-    return LineChart(
-      LineChartData(
-        lineBarsData: [
-          LineChartBarData(
-            spots: pointList,
-            isCurved: true,
-            dotData: FlDotData(
-              show: false,
-            ),
-            belowBarData: BarAreaData(
-              show: true,
-              gradient: const LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromRGBO(13, 200, 212, 0.48),
-                  Color.fromRGBO(13, 200, 212, 0),
+            var maxYValue =
+                chartData.results.entries.map((e) => e.value.toDouble()).toList().reduce(max).ceilToDouble();
+            if (maxYValue == 0) {
+              maxYValue = 1;
+            }
+
+            final maxXValue = chartData.results.entries
+                .map((e) => e.key.millisecondsSinceEpoch.toDouble())
+                .toList()
+                .reduce(max)
+                .toDouble();
+
+            final minXValue = chartData.results.entries
+                .map((e) => e.key.millisecondsSinceEpoch.toDouble())
+                .toList()
+                .reduce(min)
+                .toDouble();
+
+            return LineChart(
+              LineChartData(
+                lineBarsData: [
+                  LineChartBarData(
+                    spots: pointList,
+                    isCurved: true,
+                    dotData: FlDotData(
+                      show: false,
+                    ),
+                    belowBarData: BarAreaData(
+                      show: true,
+                      gradient: const LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color.fromRGBO(13, 200, 212, 0.48),
+                          Color.fromRGBO(13, 200, 212, 0),
+                        ],
+                      ),
+                    ),
+                  ),
                 ],
+                gridData: FlGridData(
+                  show: false,
+                ),
+                borderData: FlBorderData(
+                  show: false,
+                ),
+                minX: minXValue,
+                maxX: maxXValue,
+                minY: 0,
+                maxY: maxYValue,
+                titlesData: FlTitlesData(
+                  topTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  rightTitles: AxisTitles(
+                    sideTitles: SideTitles(showTitles: false),
+                  ),
+                  leftTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      interval: maxYValue / 5,
+                      getTitlesWidget: leftTitleWidgets,
+                      reservedSize: 40,
+                    ),
+                  ),
+                  bottomTitles: AxisTitles(
+                    sideTitles: SideTitles(
+                      showTitles: true,
+                      reservedSize: 30,
+                      getTitlesWidget: bottomTitleWidgets,
+                      interval: calculateSpacing(100, selectedTimeFrame as double),
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-        ],
-        gridData: FlGridData(
-          show: false,
-        ),
-        borderData: FlBorderData(
-          show: false,
-        ),
-        minX: minXValue,
-        maxX: maxXValue,
-        minY: 0,
-        maxY: maxYValue,
-        titlesData: FlTitlesData(
-          topTitles: AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
-          rightTitles: AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
-          leftTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: true,
-              interval: maxYValue / 5,
-              getTitlesWidget: leftTitleWidgets,
-              reservedSize: 40,
-            ),
-          ),
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(
-              showTitles: true,
-              reservedSize: 30,
-              getTitlesWidget: bottomTitleWidgets,
-              interval: (maxXValue - minXValue) / 5,
-            ),
-          ),
-        ),
-      ),
-      swapAnimationDuration: const Duration(milliseconds: 150),
-      // Optional
-      swapAnimationCurve: Curves.ease,
+              swapAnimationDuration: const Duration(milliseconds: 150),
+              swapAnimationCurve: Curves.ease,
+            );
+          },
+          error: (error, stack) {
+            return const Text('Oops, something unexpected happened');
+          },
+          loading: () {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        );
+      },
     );
   }
 }
