064730e8faee6cda802b810d3b071d070836ff9e
Kalervo Hyyppa <kalervohyyppa@gmail.com>
2023-12-15 14:39:56 -0700

    Updates to luanch url provider

--- lib/shared/providers/url_launcher_provider.dart
+++ lib/shared/providers/url_launcher_provider.dart
@@ -1,14 +1,7 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class UrlLauncher {
-  Future<bool> launchURL(Uri url) async {
-    launchUrl(url);
-    return true;
-  }
-}
-
 // This provider is used so that in tests we can mock the DateTime.now() function
-final urlLauncherProvider = Provider<UrlLauncher>((ref) {
-  return UrlLauncher();
+final urlLauncherProvider = Provider<Future<bool> Function(Uri)>((ref) {
+  return (Uri url) => launchUrl(url);
 });
